fcb7aa9697b90d1376330883f52b240b46ac5df9
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-11 21:50:28 +0200

    chore(sfpp.ch01): start groupContinuousNums

--- src/main/kotlin/org/vld/sfpp/ch01/Main.kt
+++ src/main/kotlin/org/vld/sfpp/ch01/Main.kt
@@ -3,11 +3,19 @@ package org.vld.sfpp.ch01
 /**
  * Calculates letter frequency for a string [str]
  */
-fun letterFrequency(str: String) = str
+fun letterFrequency(str: String): Map<Char, Int> = str
         .filter(Char::isLetter)
         .groupBy { it }
         .map { (letter, occurrences) -> letter to occurrences.size }
         .toMap()
+
+/**
+ * Given a sorted list of numbers [nums], calculates continuous groups of numbers,
+ * so each number in a group is higher by on than its predecessor
+ */
+fun groupContinuousNums(nums: List<Number>) {
+
+}
 
 fun main(args: Array<String>) {
     val res = letterFrequency("hello world!")
