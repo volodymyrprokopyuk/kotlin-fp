7859025480c7fd232a70c6c49981c7b9e69f5252
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-13 21:00:08 +0200

    chore(sfpp.ch02): continue Node

--- src/main/kotlin/org/vld/sfpp/ch02/Main.kt
+++ src/main/kotlin/org/vld/sfpp/ch02/Main.kt
@@ -2,6 +2,14 @@ package org.vld.sfpp.ch02
 
 fun main(args: Array<String>) {
     val tree = Node(30)
-    tree.insert(20)
+//    tree.insert(20)
+//    tree.insert(40)
+//    tree.insert(4)
+//    tree.insert(400)
+
+    insert(20, tree)
+    insert(40, tree)
+    insert(4, tree)
+    insert(400, tree)
     println(tree)
 }
--- src/main/kotlin/org/vld/sfpp/ch02/Node.kt
+++ src/main/kotlin/org/vld/sfpp/ch02/Node.kt
@@ -2,10 +2,43 @@ package org.vld.sfpp.ch02
 
 data class Node<T : Comparable<T>>(val value: T, var left: Node<T>? = null, var right: Node<T>? = null) {
 
-    fun insert(newValue: T) {
+    fun insertX(newValue: T) {
         when (value.compareTo(newValue)) {
-            1 -> left?.insert(newValue) ?: (left = Node(newValue))
-            -1 -> right?.insert(newValue) ?: (right = Node(newValue))
+            1 -> left?.insertX(newValue) ?: insertLeft(newValue)
+            -1 -> right?.insertX(newValue) ?: insertRight(newValue)
         }
     }
+
+    fun insertLeft(newValue: T) {
+        left = Node(newValue)
+    }
+
+    fun insertRight(newValue: T) {
+        right = Node(newValue)
+    }
+
+    fun insertY(newValue: T) {
+        if (newValue < value && left == null) left = Node(newValue)
+        if (newValue > value && right == null) right = Node(newValue)
+        if (newValue < value) left?.insertY(newValue)
+        if (newValue > value) right?.insertY(newValue)
+    }
 }
+
+fun <T : Comparable<T>> insert(newValue: T, tree: Node<T>?): Node<T>? {
+    if (newValue < tree?.value && tree?.left == null) {
+        tree?.left = Node(newValue)
+        return tree;
+    }
+    if (newValue > tree.value && tree?.right == null) {
+        tree?.right = Node(newValue)
+        return tree;
+    }
+    if (newValue < tree.value) {
+        return insert(newValue, tree.left)
+    }
+    if (newValue > tree.value) {
+        return insert(newValue, tree.right)
+    }
+}
+
