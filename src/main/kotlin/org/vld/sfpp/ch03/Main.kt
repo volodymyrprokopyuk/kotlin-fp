65795cd3d76fba2f9ff0b053e4a77093f38bc8f2
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-17 21:05:53 +0200

    feat(sfpp.ch03): add listNthElement

--- src/main/kotlin/org/vld/sfpp/ch03/Main.kt
+++ src/main/kotlin/org/vld/sfpp/ch03/Main.kt
@@ -4,4 +4,21 @@ package org.vld.sfpp.ch03
  * Calculates recursively the [size] of the [list]
  */
 tailrec
-fun <T> listSize(list: List<T>, size: Int = 0): Int = if (list.isEmpty()) size else listSize(list.drop(1), size + 1)
+fun <T> listSize(list: List<T>, size: Int = 0): Int =
+        // base case: the list is empty, the size of the list is calculated
+        if (list.isEmpty()) size
+        // recursive case: drop the first element from the list and increment by 1 the size of the list
+        else listSize(list.drop(1), size + 1)
+
+/**
+ * Retrieves the [nthElement] from the [list] or returns null if the index is out of range
+ */
+tailrec
+fun <T> listNthElement(list: List<T>, nthElement: Int): T? = when {
+    // positive base case: the list is not empty and the Nth element is 0
+    list.isNotEmpty() && nthElement == 0 -> list.first()
+    // negative base case: the list is empty and the Nth element is out of range
+    list.isEmpty() || nthElement >= list.size || nthElement < 0 -> null
+    // recursive case: drop the first element from the list and decrement by 1 the Nth element
+    else -> listNthElement(list.drop(1), nthElement - 1)
+}
--- src/test/kotlin/org/vld/sfpp/ch03/MainSpek.kt
+++ src/test/kotlin/org/vld/sfpp/ch03/MainSpek.kt
@@ -13,20 +13,64 @@ object MainSpek : Spek({
         val list = listOf(1, 2, 3, 4)
         on("call listSize on the non empty list") {
             val size = listSize(list)
-            it("should return size of the list") {
+            it("should return the size of the list") {
                 val expectedSize = list.size
                 assertThat(size, equalTo(expectedSize))
             }
         }
     }
 
-    given("an empty lsit") {
+    given("an empty list") {
         val list = listOf<Int>()
         on("call listSize on the empty list") {
             val size = listSize(list)
             it("should return 0 as size of the list") {
                 val expectedSize = list.size
                 assertThat(size, equalTo(expectedSize))
+            }
+        }
+    }
+
+    given("a large list") {
+        val list = (0..100).toList()
+        on("call listSize on the large list") {
+            val size = listSize(list)
+            it("should return the size of the large list") {
+                val expectedSize = list.size
+                assertThat(size, equalTo(expectedSize))
+            }
+        }
+    }
+
+    given("a non empty list") {
+        val list = listOf(1, 2, 3, 4)
+        on("call listNthElement with an index withing the range") {
+            val element = listNthElement(list, 1)
+            it("should return the Nth element") {
+                val expectedElement = list[1]
+                assertThat(element, equalTo(expectedElement))
+            }
+        }
+    }
+
+    given("an empty list") {
+        val list = listOf<Int>()
+        on("call listNthElement on the empty list") {
+            val element = listNthElement(list, 1)
+            it("should return null") {
+                val expectedElement: Int? = null
+                assertThat(element, equalTo(expectedElement))
+            }
+        }
+    }
+
+    given("a non empty list") {
+        val list = listOf(1, 2, 3, 4)
+        on("call listNthElement with an index out of the range") {
+            val element = listNthElement(list, 10)
+            it("should return null") {
+                val expectedElement: Int? = null
+                assertThat(element, equalTo(expectedElement))
             }
         }
     }
