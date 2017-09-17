1ad4279da4bda30e14ae650490af2d062d97996d
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-17 18:19:48 +0200

    feat(sfpp.ch02): add Car documentation

--- src/main/kotlin/org/vld/sfpp/ch02/Car.kt
+++ src/main/kotlin/org/vld/sfpp/ch02/Car.kt
@@ -1,7 +1,18 @@
 package org.vld.sfpp.ch02
 
+/**
+ * Car data class
+ */
 data class Car(val make: String, val model: String, val year: Int, val hasGps: Boolean = false, val hasAc: Boolean = false)
 
+/**
+ * Car validation function to be used with Car::apply()
+ *
+ * Example:
+ * ```kotlin
+ * val x5 = Car("BMW", "X5", 2016, hasGps = true, hasAc = true).apply(Car::validate)
+ * ```
+ */
 fun Car.validate() {
     if (make.isEmpty()) throw IllegalArgumentException("Car::make shouldn't be empty: $this")
     if (model.isEmpty()) throw IllegalArgumentException("Car::model shouldn't be empty: $this")
