73e43fdcc29de0312d64436bf4a42e543c2a011e
Volodymyr Prokopyuk <volodymyr.prokopyuk@ba.com>
2017-09-20 17:38:48 +0200

    test(sfpp.ch03): add ParserTest

--- src/test/kotlin/org/vld/sfpp/ch02/CarTest.kt
+++ src/test/kotlin/org/vld/sfpp/ch02/CarTest.kt
@@ -3,7 +3,9 @@ package org.vld.sfpp.ch02
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CarTest {
 
     @Test
