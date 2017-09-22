d6190fcb0171b522c8a404642b8b05073dedaa0e
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-22 22:55:35 +0200

    feat(sfpp.ch04): add eratosthenesSieve

--- src/test/kotlin/org/vld/sfpp/ch04/MainTest.kt
+++ src/test/kotlin/org/vld/sfpp/ch04/MainTest.kt
@@ -7,13 +7,23 @@ import org.junit.jupiter.api.Test
 class MainTest {
 
     @Test
-    @DisplayName("When fibonacciSequence. Then return Fibonacci sequence")
+    @DisplayName("When fibonacciSequence. Then return the Fibonacci sequence")
     fun whenFibonacciSequence_thenReturnFibonacciSequence() {
         // Given & When
-        val fibonacci = fibonacciSequence().take(12).toList()
+        val fibonacci = fibonacciSequence().take(14).toList()
         // Then
-        val expectedFibonacci = listOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+        val expectedFibonacci = listOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233)
         assertThat(fibonacci).isEqualTo(expectedFibonacci)
+    }
+
+    @Test
+    @DisplayName("When eratosthenesSieve. Then return the sequence of prime numbers")
+    fun whenEratosthenesSieve_thenReturnPrimeNumbersSequence() {
+        // Given & When
+        val primes = eratosthenesSieve().take(14).toList()
+        // Then
+        val expectedPrimes = listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43)
+        assertThat(primes).isEqualTo(expectedPrimes)
     }
 
 }
