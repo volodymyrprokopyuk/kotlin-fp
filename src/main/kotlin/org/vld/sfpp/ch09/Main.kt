42cbaa1afed235c6988a71600af3804712954e90
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-09-26 17:39:36 +0200

    feat(sfpp.ch09): add buildTitleWordToAuthorInvertedIndex

--- src/main/kotlin/org/vld/sfpp/ch09/Main.kt
+++ src/main/kotlin/org/vld/sfpp/ch09/Main.kt
@@ -32,14 +32,49 @@ fun <T, R> listMonad(transform: (T) -> List<R>, source: List<T>, destination: Li
 //        else listMonad(transform, source.drop(1), destination + transform(source.first()))
 
         // Option 2: explicit flatten operation of the destination list
-        else listMonad(transform, source.drop(1), transform(source.first()).fold(destination) {
-            destinationAccumulator, element -> destinationAccumulator + element
+        else listMonad(transform, source.drop(1), transform(source.first()).fold(destination) { destinationAccumulator, element ->
+            destinationAccumulator + element
         })
 
+/**
+ * Given an [authorToTitleList] builds an title word to author inverted index for searching author by a title word
+ */
+fun buildTitleWordToAuthorInvertedIndex(authorToTitleList: List<Pair<String, String>>): Map<String, Set<String>> =
+        authorToTitleList
+                // flatten list of pairs title word to author
+                .flatMap { (author, title) ->
+                    // split title into separate words
+                    Regex("""\W""").split(title)
+                            // filter not semantic title words
+                            .filter { titleWord -> titleWord !in setOf("in", "for", "with") }
+                            // create list of pairs title word to author
+                            .map { titleWord -> titleWord to author }
+                }
+                // group authors by title word
+                .groupBy(keySelector = { it.first }, valueTransform = { it.second })
+                // remove author duplicates by converting authors from list to set
+                .map { (titleWord, authors) -> titleWord to authors.toSet() }.toMap()
+
 fun main(args: Array<String>) {
-    val source = listOf(1, 2, 3, 4)
-    val res1 = listFunctor({ element: Int -> element * 10 }, source)
-    println(res1)
-    val res2 = listMonad({ element: Int -> listOf(element * 10) }, source)
-    println(res2)
+//    val source = listOf(1, 2, 3, 4)
+//    val res1 = listFunctor({ element: Int -> element * 10 }, source)
+//    println(res1)
+//    val res2 = listMonad({ element: Int -> listOf(element * 10) }, source)
+//    println(res2)
+
+    val authorToTitleList = listOf(
+            Pair("Dmitry Jemerov", "Kotlin in Action"),
+            Pair("Dmitry Jemerov", "Kotlin in Action"),
+            Pair("Svetlana Isakova", "Kotlin in Action"),
+            Pair("Antonio Leiva", "Kotlin for Android Developers"),
+            Pair("Denis Kalinin", "Modern Web Development with Kotlin"),
+            Pair("Stephen Samuel", "Programming Kotlin"),
+            Pair("Stefan Bocutiu", "Programming Kotlin"),
+            Pair("Milos Vasic", "Fundamental Kotlin"),
+            Pair("Marcin Moskala", "Android Development with Kotlin"),
+            Pair("Igor Wojda", "Android Development with Kotlin")
+    )
+    val res = buildTitleWordToAuthorInvertedIndex(authorToTitleList);
+    println(res["Kotlin"])
+    println(res["Android"])
 }
--- src/test/kotlin/org/vld/sfpp/ch09/MainTest.kt
+++ src/test/kotlin/org/vld/sfpp/ch09/MainTest.kt
@@ -11,6 +11,19 @@ import java.util.stream.Stream
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MainTest {
 
+    val authorToTitleList = listOf(
+            Pair("Dmitry Jemerov", "Kotlin in Action"),
+            Pair("Dmitry Jemerov", "Kotlin in Action"),
+            Pair("Svetlana Isakova", "Kotlin in Action"),
+            Pair("Antonio Leiva", "Kotlin for Android Developers"),
+            Pair("Denis Kalinin", "Modern Web Development with Kotlin"),
+            Pair("Stephen Samuel", "Programming Kotlin"),
+            Pair("Stefan Bocutiu", "Programming Kotlin"),
+            Pair("Milos Vasic", "Fundamental Kotlin"),
+            Pair("Marcin Moskala", "Android Development with Kotlin"),
+            Pair("Igor Wojda", "Android Development with Kotlin")
+    )
+
     @DisplayName("Given a transform operation and a source list. When listFunctor. Then return transformed source list")
     @ParameterizedTest(name = "{1} -> {2}")
     @MethodSource("functorSourceListProvider")
@@ -46,6 +59,28 @@ class MainTest {
         return Stream.of(
                 Arguments.of(transform, listOf<Int>(), listOf<Int>()),
                 Arguments.of(transform, listOf(1, 2, 3, 4), listOf(10, 20, 30, 40))
+        )
+    }
+
+    @DisplayName("Given an author to title list. When buildTitleWordToAuthorInvertedIndex. Then return title word to author inverted index")
+    @ParameterizedTest(name = "{0} -> {1}")
+    @MethodSource("titleWordSearchProvider")
+    fun givenAuthorToTitleList_whenBuildTitleWordToAuthorInvertedIndex_thenReturnTitleWordToAuthorInvertedIndex(
+            titleWord: String, expectedResult: Set<String>
+    ) {
+        // Given & When
+        val invertedIndex = buildTitleWordToAuthorInvertedIndex(authorToTitleList)
+        val result = invertedIndex[titleWord]
+        // Then
+        assertThat(result).isEqualTo(expectedResult)
+    }
+
+    fun titleWordSearchProvider(): Stream<Arguments> {
+        return Stream.of(
+                Arguments.of("Kotlin", setOf(
+                        "Dmitry Jemerov", "Svetlana Isakova", "Antonio Leiva", "Denis Kalinin", "Stephen Samuel",
+                        "Stefan Bocutiu", "Milos Vasic", "Marcin Moskala", "Igor Wojda")),
+                Arguments.of("Android", setOf("Antonio Leiva", "Marcin Moskala", "Igor Wojda"))
         )
     }
 
